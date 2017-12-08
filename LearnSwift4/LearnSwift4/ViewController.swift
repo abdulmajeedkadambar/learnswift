f8a415d9183ecef730997b028ee2db362173af5d
abdul <abdulkadambar@gmail.com>
2017-12-08 08:03:07 +0800

    Initial code base

---
+++ LearnSwift4/LearnSwift4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnSwift4
+//
+//  Created by Abdul Majeed K on 08/12/17.
+//  Copyright © 2017 Wafa Fathima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
