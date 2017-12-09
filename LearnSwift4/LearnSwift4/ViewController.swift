d90cf5408e30be54e79b3a05a13952c7c508f4e6
abdul <abdulkadambar@gmail.com>
2017-12-09 19:59:48 +0800

    Tableview dynamic cell

--- LearnSwift4/LearnSwift4/ViewController.swift
+++ LearnSwift4/LearnSwift4/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol ReusableView : class {
+    static var reuseIdentifier : String {get}
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+extension ReusableView where Self: UITableViewCell  {
+    static var reuseIdentifier : String {
+        return String(describing: self)
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension UITableViewCell : ReusableView {}
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    let dataSource : [String] = ["Abdul Majeed Kadambar Bangalore Singapore Mangalore", "Abdul Razak","Wafa Fathima", "Sabeena Roshni","Chabbi"]
+    
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.reuseIdentifier)
+            
+            self.tableView.dataSource = self
+            self.tableView.delegate = self
+            self.tableView.estimatedRowHeight = 44
+            self.tableView.rowHeight = UITableViewAutomaticDimension
+        }
     }
-
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.reuseIdentifier)
+        cell?.textLabel?.numberOfLines = 0
+        cell?.textLabel?.text = dataSource[indexPath.row]
+        return cell!
+    }
+    
 }
 
